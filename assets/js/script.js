619345494793ef1b850e8672fb92da4aff2dfc9a
Ajfriel86 <angus.friel.1986@gmail.com>
2021-08-23 09:06:32 +0000

    added functions event listeners and functions for gamePlay instructions, to see if two cards are fillped and/or a match or not

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,76 @@ Varibles
  let firstCard, secondCard; 
  let moves = 0;
  let finalTime = "";
+
+ /*
+ Event Listeners
+ */
+ cards.forEach(card => card.addEventListener('click', flipCard)); 
+ shuffle();
+ 
+ resetBtn.addEventListener('click', showGamePlay); 
+ closeBtn.addEventListener('click', closeGamePaly);
+ 
+ /*
+ Function to show and close gamePlay 
+ */
+ function showGamePaly() {
+     gamePlay.style.display = 'block';
+ }
+ 
+ function closeGamePlay() {
+     gamePaly.style.display = 'none';
+ }
+/*
+Function for two cards to be flipped
+*/
+ function flipCard() {
+    if (!gameOn) {
+        gameOn = true;
+        timer();
+    }
+    if (lockBoard) return; 
+    if (this === firstCard) return;
+
+    this.classList.add('flip'); 
+
+    if (!flippedCard) { 
+
+        flippedCard = true;
+        firstCard = this; 
+
+        return;
+    }
+    secondCard = this; 
+    checkCardMatch();
+}
+/*
+Function to check if cards match or not
+*/
+function checkCardMatch() {
+    let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+    if (isMatch) perfectMatch += 1;
+
+    addMove();
+    
+    if (isMatch) pairMatch();
+    else noMatch();
+
+    if (perfectMatch === MAX_MATCH) winGame();
+}
+function pairMatch() {
+
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+function noMatch() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+    }, 900);
+}
